d29eb642677cfcc44d0eb280ee08f65a9fd0bbd3
Paul Fortin <pfortin@urbn.com>
2021-05-17 15:35:53 -0400

    Update server.go

--- server.go
+++ server.go
@@ -1,12 +1,18 @@
 package main
 
 import (
+	"fmt"
+	"os"
 	"net/http"
 	
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
+  port := os.Getenv("PORT")
+  if port == "" {
+    port = "8080"	  
+  }
   e := echo.New()
   e.GET("/", func(c echo.Context) error {
     return c.String(http.StatusOK, "Hello, World!")
@@ -16,5 +22,5 @@ func main() {
       "status": "ok",
     })
   })
-  e.Logger.Fatal(e.Start(":1323"))
+  e.Logger.Fatal(e.Start(fmt.Sprintf(":%s", port)))
 }
